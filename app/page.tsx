42fcc40df321708cfd65594db2d3b6d8d6549d50
echo <play3step@naver.com>
2025-12-12 16:57:40 +0900

    [style] 하단 dock 및 배경 이미지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { BottomDock } from "@/src/widgets";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <h1>Hello World</h1>
+      <div className="w-full max-w-md absolute bottom-10 left-1/2 -translate-x-1/2">
+        <BottomDock />
+      </div>
     </div>
   );
 }
---
+++ src/widgets/bottom-dock/ui/BottomDock.tsx
@@ -0,0 +1,7 @@
+const BottomDock = () => {
+  return (
+    <section className="w-full max-w-md h-16 bg-white/10 backdrop-blur-2xl border border-white/20 shadow-[0_8px_32px_rgba(0,0,0,0.37)] flex items-center justify-center gap-3 px-4 py-3 rounded-2xl"></section>
+  );
+};
+
+export default BottomDock;
