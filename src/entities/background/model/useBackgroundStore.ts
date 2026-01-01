3f4c28a28d2ecf4c3a14e14341c43d581a091bfa
echo <play3step@naver.com>
2026-01-01 17:03:54 +0900

    [fix] 배경이미지 Url 오류 수정

--- src/entities/background/model/useBackgroundStore.ts
+++ src/entities/background/model/useBackgroundStore.ts
@@ -1,3 +1,4 @@
+import { BackgroundImages } from "@/src/shared";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
@@ -7,10 +8,10 @@ interface BackgroundState {
 }
 
 export const backgrounds = [
-  { id: "bg1", path: "/background1.jpg", name: "Background 1" },
-  { id: "bg2", path: "/background2.png", name: "Background 2" },
-  { id: "bg3", path: "/background3.png", name: "Background 3" },
-  { id: "bg4", path: "/background4.png", name: "Background 4" },
+  { id: "bg1", path: BackgroundImages.background1, name: "Background 1" },
+  { id: "bg2", path: BackgroundImages.background2, name: "Background 2" },
+  { id: "bg3", path: BackgroundImages.background3, name: "Background 3" },
+  { id: "bg4", path: BackgroundImages.background4, name: "Background 4" },
 ];
 
 export const useBackgroundStore = create<BackgroundState>()(
